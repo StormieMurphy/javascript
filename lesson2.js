cd44253b836e21e5f0facef7f40c1a47d56c4020
StormieMurphy <stolenmurphy@gmail.com>
2025-05-31 20:39:03 -0500

    lesson 2 lab

---
+++ lesson2.js
@@ -0,0 +1,16 @@
+const username = 'John Smith';
+let fruit = 'Mango';
+fruit = 'Grape';
+
+// console.log(username);
+// console.log(fruit);
+// console.log("Hello World!")
+
+const numberOne = 1;
+const amount = 500;
+
+console.log(numberOne);
+console.log(amount);
+
+const FULL_NAME = "John Doe";
+const AMOUNT = 1000;
