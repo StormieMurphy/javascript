fe711d8ac2b3e72c6d2c1d59d4bcb3ce769bb445
StormieMurphy <stolenmurphy@gmail.com>
2025-06-02 18:47:15 -0500

    Lesson 3 Part 1

---
+++ string.js
@@ -0,0 +1,22 @@
+const longstring = "Hello, learners!" + 
+                   "Welcome to Javascript Fundamentals!" +
+                   "This practice will teach you the basics of Javascript strings.";
+
+const anotherlongstring = 'Hello, learners!' + 
+                          'Welcome to Javascript Fundamentals!' +
+                          'This practice will teach you the basics of Javascript strings.';
+
+const anotherlongstringTwo = 'Hello, learners!\
+                          Welcome to Javascript Fundamentals!\
+                          This practice will teach you the basics of Javascript strings.';
+
+let alpha = "Hello";
+let beta = " world!";
+let number = 10;
+
+// let combinedVariables = alpha + beta;\
+let combinedVariables = alpha + number;
+
+console.log(combinedVariables);
+// console.log(alpha);
+// console.log(beta);
