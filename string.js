901ffdd32cdeed29ee08c4ea3d24c064c6bbbabf
StormieMurphy <stolenmurphy@gmail.com>
2025-06-02 19:16:23 -0500

    Lesson 3 Part 2

--- string.js
+++ string.js
@@ -11,12 +11,31 @@ const anotherlongstringTwo = 'Hello, learners!\
                           This practice will teach you the basics of Javascript strings.';
 
 let alpha = "Hello";
+// let alpha = "10";
 let beta = " world!";
 let number = 10;
 
-// let combinedVariables = alpha + beta;\
+// let combinedVariables = alpha + beta;
 let combinedVariables = alpha + number;
+const combinedVariablesTwo = "I say" + alpha + ' ' + beta + number + "times.";
+// console.log(combinedVariablesTwo)
+// alpha + number + "Hello" + 10
+// console.log("1" + "1")
+// console.log(1 + 1)
 
-console.log(combinedVariables);
+// console.log(combinedVariables);
 // console.log(alpha);
 // console.log(beta);
+
+const VALUE_ONE = 10;
+const VALUE_TWO = 20;
+
+// console.log("Thirty is " + (VALUE_ONE + VALUE_TWO) + " and not " + (2 * VALUE_ONE + VALUE_TWO));
+// console.log("Thirty is " + VALUE_ONE + VALUE_TWO + " and not " + 2 * VALUE_ONE + VALUE_TWO)
+
+// console.log(`Thirty is ${VALUE_ONE + VALUE_TWO} and not ${2 * VALUE_ONE + VALUE_TWO}`);
+
+// console.log(`${VALUE_TWO} is greater than ${VALUE_ONE}`);
+
+console.log('It\'s a cat.');
+console.log("It's a cat.");
