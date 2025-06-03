f05ec4854984a8218ac4a018db140970598e7718
StormieMurphy <stolenmurphy@gmail.com>
2025-06-03 18:55:07 -0500

    Lesson 3 Part 3

--- string.js
+++ string.js
@@ -1,6 +1,6 @@
-const longstring = "Hello, learners!" + 
+const longstring = "Hello, learners! " + 
                    "Welcome to Javascript Fundamentals!" +
-                   "This practice will teach you the basics of Javascript strings.";
+                   " This practice will teach you the basics of Javascript strings.";
 
 const anotherlongstring = 'Hello, learners!' + 
                           'Welcome to Javascript Fundamentals!' +
@@ -37,5 +37,15 @@ const VALUE_TWO = 20;
 
 // console.log(`${VALUE_TWO} is greater than ${VALUE_ONE}`);
 
-console.log('It\'s a cat.');
-console.log("It's a cat.");
+// console.log('It\'s a cat.');
+// console.log("It's a cat.");
+
+console.log(longstring.length);
+const name = "Carl";
+console.log(name.length);
+
+console.log(name.at(-2))
+
+// C, a, r, l -> String
+// 0, 1, 2, 3 -> Indexing starts from left to right.
+// -4,-3,-2,-1 -> Indexing starts from right to left.
