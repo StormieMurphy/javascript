7b1d3cf909980675944a09ee6dbaa43e42713de5
StormieMurphy <stolenmurphy@gmail.com>
2025-06-06 12:54:22 -0500

    Lesson 5 Part 1

---
+++ functions.js
@@ -0,0 +1,23 @@
+function simpleFunction(){
+    console.log("Hello Javascript Function!");
+    // this is a simple function which is a building block of JS.
+}
+// simpleFunction();
+
+// The function below returns a string using the return keyword.
+function simpleFunctionTwo(){
+    return "I am from simpleFunctionTwo!"
+}
+
+// console.log(simpleFunctionTwo());
+// this function takes an input and stores it in the valueOne parameter.
+function simpleFunctionThree(valueOne){
+    console.log(valueOne);
+}
+// simpleFunctionThree("Dog");
+
+// This is the function expression basically meaning assigning a function to a variable.
+const functionVariable = simpleFunctionTwo();
+// 
+
+console.log(functionVariable);
