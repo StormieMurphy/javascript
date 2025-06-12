d11ece11a9de5a68b5f1119463f8cbdaf1dd713f
StormieMurphy <stolenmurphy@gmail.com>
2025-06-12 11:43:18 -0500

    Lesson 8 Parts 2-4

--- loops.js
+++ loops.js
@@ -6,4 +6,23 @@ const whileExample = () => {
         counter++;
     };
 };
-whileExample();
+// whileExample();
+
+// Do-while loop
+const doWhileExample = () => {
+    let counter = 0;
+    do {
+        console.log(counter);
+        counter++;
+    }  while (counter < 5);
+}
+// doWhileExample();
+
+// For loop
+const forLoopExample = () => {
+    for(counter = 0; counter <= 20; counter++){
+        if(counter % 2 == 0) console.log(`Number ${counter} is even.`);
+
+    };
+}
+forLoopExample();
