135c2d7a70ddd82a6228625e1314f5b8f6f75cd2
StormieMurphy <stolenmurphy@gmail.com>
2025-06-23 18:48:45 -0500

    Lesson 10 Part 1

---
+++ maps.js
@@ -0,0 +1,14 @@
+// const programmingLanguages = new Map();
+// programmingLanguages.set("JS", "JavaScript");
+// programmingLanguages.set("PY", "Python");
+// programmingLanguages.set("JA", "Java");
+// programmingLanguages.set("RR", "Ruby on Rails");
+
+const programmingLanguages = new Map([
+    ["JS", "JavaScript"], 
+    ["PY", "Python"], 
+    ["JA", "Java"],
+    ["RR", "Ruby on Rails"]
+]);
+
+console.log(programmingLanguages);
